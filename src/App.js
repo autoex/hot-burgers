97f101a0cf4fa0adbd25c0210c44484960627aa5
autoex <autoex@gmail.com>
2021-10-14 11:50:01 +0300

    connect addBurger form with global state in App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import Order from "./components/Order";
 import MenuAdmin from "./components/MenuAdmin";
 
 class App extends Component {
+    state = {
+        burgers: {},
+        order: {}
+    };
+    addBurger =(burger)=> {
+        const burgers = {...this.state.burgers};
+        burgers[`burger-${Date.now()}`] = burger;
+        this.setState({burgers})
+    };
     render() {
         return (
             <div className='burger-paradise'>
@@ -11,7 +20,7 @@ class App extends Component {
                     <Header/>
                 </div>
                 <Order/>
-                <MenuAdmin/>
+                <MenuAdmin addBurger={this.addBurger}/>
 
             </div>
         );
