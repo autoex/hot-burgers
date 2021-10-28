158a12879898940207a0f47f6422eb48015390e2
autoex <autoex@gmail.com>
2021-10-28 19:59:41 +0300

    add animation with React Transition Group

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import Shipment from "./Shipment";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 class Order extends React.Component {
     renderOrder = (item) => {
         const burger = this.props.burgers[item];
         const count = this.props.order[item];
         const isAvailable = burger && burger.status === 'available';
-        if(!burger) return;
-        if (!isAvailable) return <li key={item} className='unavailable'>`Извените, бургер не доступен`</li>;
-        return <li key={item}>
-                <span>
-                    <span>{count} шт. </span>
-                    <span>{burger.name} </span>
-                    <span>{burger.price * count} ₽</span>
-                    <button onClick={()=>this.props.deleteFromOrder(item)} className='cancelItem'>&times; </button>
-                </span></li>
+        if (!burger) return;
+        if (!isAvailable) {
+            return <CSSTransition key={item} timeout={500} classNames="order">
+                <li key={item} className='unavailable'>`Извените, бургер не доступен`</li>
+            </CSSTransition>
+        }
+
+        return <CSSTransition key={item} timeout={500} classNames="order">
+            <li key={item}>
+                <TransitionGroup component={'span'} className={'count'}>
+                    <CSSTransition classNames={'count'} key={count} timeout={500}>
+                        <span>{count}</span></CSSTransition>
+                </TransitionGroup>
+                <span>шт. {burger.name}&nbsp;</span>
+                <span>{burger.price * count} ₽</span>
+                <button onClick={() => this.props.deleteFromOrder(item)} className='cancelItem'>&times; </button>
+            </li>
+        </CSSTransition>
     };
 
     render() {
@@ -30,13 +40,13 @@ class Order extends React.Component {
         return (
             <div className='order-wrap'>
                 <h2>Ваш заказ</h2>
-                <ul className='order'>
+                <TransitionGroup component={'ul'} className='order'>
                     {orderIds.map(this.renderOrder)}
-                </ul>
+                </TransitionGroup>
                 {totalCost > 0 ?
                     <Shipment totalCost={totalCost}/>
                     :
-                    (<div className='nothingSelected'>Выберите блюда и добавте к заказу</div> )  }
+                    (<div className='nothingSelected'>Выберите блюда и добавте к заказу</div>)}
             </div>
         )
     }
